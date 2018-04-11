b9fa49b53dbca78e239cc927df6f6c8dc0af4f6f
Sam Zakkour <salim.zakkour@gmail.com>
2018-04-11 11:49:30 +0000

    added eventdump2

--- index.py
+++ index.py
@@ -5,7 +5,8 @@ import datetime
 def handler(event, context):
     data = {
         'output': 'Hello World AllAWS',
-        'timestamp': datetime.datetime.utcnow().isoformat()
+        'timestamp': datetime.datetime.utcnow().isoformat(),
+        'eventdump': str(json.dumps(event))
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
